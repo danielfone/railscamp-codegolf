0e70781e44355eabdad11169adbf5ee0e7ac701a
Daniel Fone <daniel@fone.net.nz>
2014-02-24 22:58:07 +1300

    Tidy leaderboard

--- webapp/code-golf/app/helpers/application_helper.rb
+++ webapp/code-golf/app/helpers/application_helper.rb
@@ -4,4 +4,15 @@ module ApplicationHelper
     score or content_tag :span, '-', class: 'text-muted'
   end
 
+  def leaderboard_header
+    html = []
+    html << content_tag(:th)
+    html << 1.upto(9).map { |hole| content_tag :th, hole }
+    html << content_tag(:th, 'OUT')
+    html << 10.upto(18).map { |hole| content_tag :th, hole }
+    html << content_tag(:th, 'IN')
+    html << content_tag(:th, 'TOTAL')
+    html.join.html_safe
+  end
+
 end
