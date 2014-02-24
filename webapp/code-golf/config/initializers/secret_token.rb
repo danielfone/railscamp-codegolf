b261a14fd85557c9f37beb15922484d4791df0c9
Daniel Fone <daniel@fone.net.nz>
2014-02-24 20:28:27 +1300

    Rails 4.0.2

---
+++ webapp/code-golf/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CodeGolf::Application.config.secret_key_base = '6a173c43f224b3e63e44f132e27a99465d2ebfdb45c1a3d15b11cd056d3eeddc3f1763ab84402d7ebdc75e24cd2ed13bea0f1e4ef6f05f4b076564d1dfeb5a5e'
---
+++ webapp/code-golf/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CodeGolf::Application.config.session_store :cookie_store, key: '_code-golf_session'
