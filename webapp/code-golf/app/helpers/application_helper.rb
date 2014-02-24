102504d510c99e0399ca3aeacfd1b03602320f4b
Daniel Fone <daniel@fone.net.nz>
2014-02-24 22:43:31 +1300

    Team scores

--- webapp/code-golf/app/helpers/application_helper.rb
+++ webapp/code-golf/app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def render_score(score)
+    score or content_tag :span, '-', class: 'text-muted'
+  end
+
 end
--- webapp/code-golf/app/models/team.rb
+++ webapp/code-golf/app/models/team.rb
@@ -1,20 +1,33 @@
 class Team < ActiveRecord::Base
 
-  def score
-    Array.new(18, 0)
+  serialize :scores, JSON
+
+  after_initialize :setup_defauls
+
+  def score_for(hole)
+    scores[hole-1]
   end
 
   def front_nine
-    score[1..9].sum
+    sum_scores scores[0..8]
   end
 
   def back_nine
-    score[10..18].sum
+    sum_scores scores[9..17]
   end
 
   def total
-    score.sum
+    sum_scores scores
   end
 
+private
+
+  def sum_scores(array)
+    array.map(&:to_i).sum unless array.compact.empty?
+  end
+
+  def setup_defauls
+    self.scores ||= Array.new(18, nil)
+  end
 
 end
