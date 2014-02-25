e986bdf8ca2bdd08b0840c9ba977e8ee6bc308c4
Daniel Fone <daniel@fone.net.nz>
2014-02-25 13:18:23 +1300

    Nicer score card

--- webapp/code-golf/app/helpers/application_helper.rb
+++ webapp/code-golf/app/helpers/application_helper.rb
@@ -1,26 +1,17 @@
 module ApplicationHelper
 
   def render_numeric_cells(numbers, opts={})
-    safe_join numbers.map { |n| render_numeric_cell n, opts}
+    safe_join numbers.map { |n| render_numeric_cell n, opts }
   end
 
   def render_numeric_cell(number, opts={})
     html_opts = {}.merge opts
+    html_tag = html_opts.delete(:tag) || :td
     html_opts[:class] = [opts[:class], 'numeric'].compact.join(' ')
-    content_tag :td, html_opts do
+
+    content_tag html_tag, html_opts do
       number_with_delimiter number or content_tag :span, '-', class: 'text-muted'
     end.html_safe
-  end
-
-  def leaderboard_header
-    html = []
-    html << content_tag(:th)
-    html << 1.upto(9).map { |hole| content_tag :th, hole, class: 'numeric hole' }
-    html << content_tag(:th, 'OUT', class: 'numeric hole active')
-    html << 10.upto(18).map { |hole| content_tag :th, hole, class: 'numeric hole' }
-    html << content_tag(:th, 'IN', class: 'numeric hole active')
-    html << content_tag(:th, 'TOTAL', class: 'numeric active')
-    html.join.html_safe
   end
 
   def meta_refresh
