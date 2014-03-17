d8bcc4efee25dada1d13b3327c5e5c8068c0a5e1
Daniel Fone <daniel@fone.net.nz>
2014-03-17 15:23:44 +1300

    Removed old holes

--- course/holes/14_applify.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby -w
-
-while line = gets do
-  word = line.chomp
-  new_word = word[0].upcase + word[1..-1]
-  puts "i" + new_word
-end
--- course/holes/16_cat.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env ruby -w
-
-STDIN.read.each_line do |line|
-  STDOUT.puts "#{line}"
-end
