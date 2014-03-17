f7fae575f15813141f2a75b17c59c2aa3c18c87a
Daniel Fone <daniel@fone.net.nz>
2014-03-17 22:04:16 +1300

    Update par

--- course/pars/19_levenshtein.rb
+++ course/pars/19_levenshtein.rb
@@ -4,3 +4,4 @@ m=[(0..s).to_a]
 1.upto(t){|j|m<<[j]+[0]*s}
 t.times{|i|s.times{|j|m[i+1][j+1]=a[j]==b[i]?m[i][j]:[m[i][j+1],m[i+1][j],m[i][j],].min+1}}
 m[-1][-1]}
+# padding so we don't make too much :'(
