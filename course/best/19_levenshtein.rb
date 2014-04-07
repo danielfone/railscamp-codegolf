b4233f61a0e5994b7c42a9136e9228f763245025
Daniel Fone <daniel@fone.net.nz>
2014-04-07 15:43:55 +1200

    Less Bytes!

--- course/best/19_levenshtein.rb
+++ course/best/19_levenshtein.rb
@@ -1,4 +1,4 @@
-puts$<.map{|l|s,t=l.split
+$<.map{|l|s,t=l.split
 m,x=t.size
 d=(0..m).to_a
 s.size.times{|i|e=i+1
@@ -6,4 +6,4 @@ m.times{|j|x=[d[j+1]+1,e+1,d[j]+(s[i]==t[j]?0:1)].min
 d[j]=e
 e=x}
 d[m]=x}
-x}
+p x}
